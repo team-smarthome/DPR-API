f6c34bfd025b1b85e64ae3dcd43f759fe7a5d2c8
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-09-30 15:45:09 +0700

    feat(lokasi): crud api

--- app/Repositories/Implementations/LokasiRepository.php
+++ app/Repositories/Implementations/LokasiRepository.php
@@ -2,19 +2,55 @@
 
 namespace App\Repositories\Implementations;
 
+use App\Http\Resources\Master\LokasiResource;
 use App\Models\Lokasi;
 use App\Repositories\Interfaces\LokasiRepositoryInterface;
+use App\Traits\ResponseTrait;
+use Dotenv\Exception\ValidationException;
+use ErrorException;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Str;
 
 class LokasiRepository implements LokasiRepositoryInterface
 {
-  public function create(array $data): Lokasi
+  use ResponseTrait;
+  public function create(array $data)
   {
-    return Lokasi::create($data);
+    $existingLokasi = Lokasi::where('nama_lokasi', $data['nama_lokasi'])->first();
+
+    if ($existingLokasi) {
+      return $this->alreadyExist('Lokasi Already Exist');
+    }
+
+    return $this->created(Lokasi::create($data));
   }
 
-  public function get(): array
+  public function get(Request $request)
   {
-    return Lokasi::all()->toArray();
+    try {
+      $collection = Lokasi::latest();
+      $keyword = $request->query("search");
+      $isNotPaginate = $request->query("not-paginate");
+
+      if ($keyword) {
+        $collection->where('nama_lokasi', 'ILIKE', "%$keyword%");
+      }
+
+      if ($isNotPaginate) {
+        $collection = $collection->get();
+        $result = LokasiResource::collection($collection)->response()->getData(true);
+        return $this->wrapResponse(Response::HTTP_OK, 'Successfully get Data', $result);
+      } else {
+        return $this->paginate($collection, null, 'Successfully get Data');
+      }
+    } catch (ValidationException $e) {
+      return $this->wrapResponse(Response::HTTP_BAD_REQUEST, $e->getMessage());
+    } catch (ErrorException $e) {
+      return $this->wrapResponse(Response::HTTP_INTERNAL_SERVER_ERROR, 'Terjadi kesalahan internal.');
+    } catch (\Throwable $th) {
+      return $this->wrapResponse(Response::HTTP_INTERNAL_SERVER_ERROR, 'Terjadi kesalahan: ' . $th->getMessage());
+    }
   }
 
   public function getById(string $id): ?Lokasi
@@ -22,19 +58,33 @@ class LokasiRepository implements LokasiRepositoryInterface
     return Lokasi::find($id);
   }
 
-  public function update(string $id, array $data): ?Lokasi
+  public function update(string $id, array $data)
   {
-    $model = Lokasi::find($id);
-    if ($model) {
-      $model->update($data);
-      return $model;
+    if (!Str::isUuid($id)) {
+      return $this->invalidUUid();
     }
-    return null;
+
+    $model = Lokasi::find($id);
+    if (!$model) {
+      return $this->notFound();
+    }
+
+    $model->update($data);
+    return $this->updated();
   }
 
-  public function delete(string $id): bool
+  public function delete(string $id)
   {
+    if (!Str::isUuid($id)) {
+      return $this->invalidUUid();
+    }
+
     $model = Lokasi::find($id);
-    return $model ? $model->delete() : false;
+    if (!$model) {
+      return $this->notFound();
+    } else {
+      $model->delete();
+      return $this->deleted();
+    }
   }
 }
