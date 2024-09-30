fb64264f820986a644ba3497129b0d526f98e4a9
sslthn31 <alhambra.sulthan@gmail.com>
2024-09-30 13:02:05 +0700

    add device zone

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,10 +7,12 @@ use App\Repositories\Implementations\DeviceRepository;
 use App\Repositories\Implementations\JabatanRepository;
 use App\Repositories\Implementations\InstansiRepository;
 use App\Repositories\Implementations\DeviceTypeRepository;
+use App\Repositories\Implementations\DeviceZoneRepository;
 use App\Repositories\Interfaces\DeviceRepositoryInterface;
 use App\Repositories\Interfaces\JabatanRepositoryInterface;
 use App\Repositories\Interfaces\InstansiRepositoryInterface;
 use App\Repositories\Interfaces\DeviceTypeRepositoryInterface;
+use App\Repositories\Interfaces\DeviceZoneRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +23,9 @@ class AppServiceProvider extends ServiceProvider
    */
   public function register()
   
+  
   {
+    $this->app->bind(DeviceZoneRepositoryInterface::class, DeviceZoneRepository::class);
     $this->app->bind(DeviceRepositoryInterface::class, DeviceRepository::class);
     $this->app->bind(DeviceTypeRepositoryInterface::class, DeviceTypeRepository::class);
     $this->app->bind(JabatanRepositoryInterface::class, JabatanRepository::class);
