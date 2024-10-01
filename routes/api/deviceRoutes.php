48abce17b9f558cb1e46ab0dba25aba02c18ad20
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-10-01 17:46:59 +0700

    wip(routes): update

--- routes/api/deviceRoutes.php
+++ routes/api/deviceRoutes.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\Master\DeviceController;
 
 $router->group(['prefix' => 'master'], function () use ($router) {
   $router->group(['middleware' => 'auth:user'], function () use ($router) {
-    $router->get('/device', 'Master\DeviceTypeController@index');
+    $router->get('/device', 'Master\DeviceController@@index');
   });
   $router->group(['middleware' => 'auth:super-admin'], function () use ($router) {
-    $router->post('/device', 'Master\DeviceTypeController@store');
-    $router->put('/device/{id}', 'Master\DeviceTypeController@update');
-    $router->delete('/device/{id}', 'Master\DeviceTypeController@destroy');
+    $router->post('/device', 'Master\DeviceController@@store');
+    $router->put('/device/{id}', 'Master\DeviceController@@update');
+    $router->delete('/device/{id}', 'Master\DeviceController@@destroy');
   });
 });
--- routes/api/devicetypeRoutes.php
+++ routes/api/devicetypeRoutes.php
@@ -7,11 +7,11 @@ use App\Http\Controllers\Master\DeviceTypeController;
 
 $router->group(['prefix' => 'master'], function () use ($router) {
   $router->group(['middleware' => 'auth:user'], function () use ($router) {
-    $router->get('/device-type', 'Master\DeviceController@index');
+    $router->get('/device-type', 'Master\DeviceTypeController@index');
   });
   $router->group(['middleware' => 'auth:super-admin'], function () use ($router) {
-    $router->post('/device-type', 'Master\DeviceController@store');
-    $router->put('/device-type/{id}', 'Master\DeviceController@update');
-    $router->delete('/device-type/{id}', 'Master\DeviceController@destroy');
+    $router->post('/device-type', 'Master\DeviceTypeController@store');
+    $router->put('/device-type/{id}', 'Master\DeviceTypeController@update');
+    $router->delete('/device-type/{id}', 'Master\DeviceTypeController@destroy');
   });
 });
