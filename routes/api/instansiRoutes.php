ae4287ac62503d97981fd97c73507d500a76ee4e
arioprima <arioprima2@gmail.com>
2024-10-02 11:02:04 +0700

    fix: route

--- routes/api/instansiRoutes.php
+++ routes/api/instansiRoutes.php
@@ -2,14 +2,13 @@
 
 use App\Http\Controllers\Master\InstansiController;
 
-// $router->get('/instansi', [InstansiController::class, 'index']);
 $router->group(['prefix' => 'master'], function () use ($router) {
   $router->group(['middleware' => 'auth:user'], function () use ($router) {
-    $router->get('/instansi', 'Master\InstansiController@@index');
+    $router->get('/instansi', 'Master\GrupPegawaiController@index');
   });
   $router->group(['middleware' => 'auth:super-admin'], function () use ($router) {
-    $router->post('/instansi', 'Master\InstansiController@@store');
-    $router->put('/instansi/{id}', 'Master\InstansiController@@update');
-    $router->delete('/instansi/{id}', 'Master\InstansiController@@destroy');
+    $router->post('/instansi', 'Master\GrupPegawaiController@store');
+    $router->put('/instansi/{id}', 'Master\GrupPegawaiController@update');
+    $router->delete('/instansi/{id}', 'Master\GrupPegawaiController@destroy');
   });
 });
