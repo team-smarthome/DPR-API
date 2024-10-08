9e729faa0b7b2aac5fdfe828f9a1b8345628a094
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-10-08 14:15:39 +0700

    wip(jabtan routes): update

--- routes/api/jabatanRoutes.php
+++ routes/api/jabatanRoutes.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\Master\JabatanController;
 
 $router->group(['prefix' => 'master'], function () use ($router) {
-  $router->group(['middleware' => 'auth:user'], function () use ($router) {
-    $router->get('/jabatan', 'Master\JabatanController@index');
-  });
+  $router->get('/jabatan', 'Master\JabatanController@index');
   $router->group(['middleware' => 'auth:super-admin'], function () use ($router) {
     $router->post('/jabatan', 'Master\JabatanController@store');
     $router->put('/jabatan/{id}', 'Master\JabatanController@update');
