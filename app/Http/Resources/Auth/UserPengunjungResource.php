450b925df28d832255175294a9fb4b7d976c5313
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-10-09 13:17:37 +0700

    wip(auth_pengunjung): change password

--- app/Http/Resources/Auth/UserPengunjungResource.php
+++ app/Http/Resources/Auth/UserPengunjungResource.php
@@ -16,6 +16,7 @@ class UserPengunjungResource extends JsonResource
   {
     return [
       'id' => $this->id,
+      'pegunjung_id' => $this->pengunjung_id,
       'username' => $this->username,
       'last_login' => $this->last_login,
       'image_url' => $this->pengunjung->facialData->face_template,
--- routes/api/authPengunjungRoutes.php
+++ routes/api/authPengunjungRoutes.php
@@ -1,4 +1,5 @@
 <?php
 $router->group(['prefix' => 'auth'], function () use ($router) {
-  $router->post('/login-pengunjung', 'Auth\LoginPengunjungController@login');
+  $router->post('/login-pengunjung', 'Auth\AuthPengunjungController@login');
+  $router->put('/change-password-pengunjung', 'Auth\AuthPengunjungController@changePassword');
 });
